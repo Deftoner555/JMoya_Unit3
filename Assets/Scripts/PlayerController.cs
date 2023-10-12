d4dacd3e747036b0739fa143e7337f44590398b6
Deftoner555 <jmoya555@gmail.com>
2023-10-11 17:24:55 -0700

    Finished Lesson C

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,21 +10,28 @@ public class PlayerController : MonoBehaviour
     private bool onGround = true;
     public bool gameOver = false;
 
+    private Animator animPlayer;
+
     void Start()
     {
         rbPlayer = GetComponent<Rigidbody>();
 
         //same as "Physics.gravity = Physics.gravity * gravityModifier"
         Physics.gravity *= gravityModifier;
+
+        animPlayer = GetComponent<Animator>();
     }
 
     void Update()
     {
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
-        if (spaceDown &&onGround)
+        //conditions met to jump
+        if (spaceDown &&onGround && !gameOver)
         {
+            //jump code
             rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            animPlayer.SetTrigger("Jump_trig");
         }
     }
 
@@ -34,10 +41,13 @@ public class PlayerController : MonoBehaviour
         {
             onGround = true;
         }
+        //Game is over when the condition is met
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             Debug.Log("Game Over!");
             gameOver = true;
+            animPlayer.SetBool("Death_b", true);
+            animPlayer.SetInteger("DeathType_int", 2);
         }
         
     }
